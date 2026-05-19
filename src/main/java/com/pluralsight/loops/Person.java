e57b61ce9db1c2a883061631089e278bbb4b555c
jolienguyen0831 <uyennguyen0831@gmail.com>
2026-05-18 23:36:49 -0500

    find matching person last name

--- src/main/java/com/pluralsight/loops/Person.java
+++ src/main/java/com/pluralsight/loops/Person.java
@@ -1,4 +1,33 @@
 package com.pluralsight.loops;
 
 public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
--- src/main/java/com/pluralsight/loops/Program.java
+++ src/main/java/com/pluralsight/loops/Program.java
@@ -1,4 +1,45 @@
 package com.pluralsight.loops;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Program {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        List<Person> people = new ArrayList<>();
+        people.add(new Person("Stephen", "Curry", 36));
+        people.add(new Person("Kevin", "Durant", 35));
+        people.add(new Person("Giannis", "Antetokounmpo", 29));
+        people.add(new Person("Joel", "Embiid", 30));
+        people.add(new Person("Luka", "Doncic", 25));
+        people.add(new Person("Jayson", "Tatum", 26));
+        people.add(new Person("Nikola", "Jokic", 29));
+        people.add(new Person("Anthony", "Davis", 31));
+        people.add(new Person("Jimmy", "Butler", 34));
+        people.add(new Person("Kawhi", "Leonard", 32));
+        people.add(new Person("Devin", "Booker", 27));
+
+        // Duplicate last names
+        people.add(new Person("Jalen", "Williams", 23));
+        people.add(new Person("Grant", "Williams", 25));
+        people.add(new Person("Jaden", "Ivey", 22));
+        people.add(new Person("Keyontae", "Johnson", 24));
+        people.add(new Person("Keldon", "Johnson", 24));
+        people.add(new Person("Draymond", "Green", 34));
+        people.add(new Person("Josh", "Green", 23));
+        people.add(new Person("Trey", "Murphy", 24));
+
+        System.out.print("Enter last name you want to search: ");
+        String lastName = input.nextLine();
+        List<Person> matchingPeople = new ArrayList<>();
+        for(Person person: people){
+            if (person.getLastName().toLowerCase().contains(lastName)){
+                matchingPeople.add(person);
+            }
+        }
+        System.out.println(matchingPeople);
+
+    }
 }
